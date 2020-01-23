3872b4103537dd531e87af19b3f8b8dc507839d3
bico steve <bicosteve4@gmail.com>
2020-01-23 18:50:37 +0300

    Creating the first get view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    '''test api view'''
+
+    def get(self,request,format=None):
+        '''returns a list of apiview features'''
+
+        api_view = [
+            'methods are post,put, get, patch',
+            'is cool to learn',
+            'mapped on urls',
+        ]
+
+        return Response({'message':'hello','api':api_view})
