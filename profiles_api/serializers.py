61041d0e23b2cd9ec844ba799dad17b87b7f25c5
bico steve <bicosteve4@gmail.com>
2020-01-27 10:55:22 +0300

    added vieset to profile_api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
-    ''' serializers  a name field for testing apiview'''
+    ''' serializes  a name field for testing apiview'''
 
     name = serializers.CharField(max_length=10)
-    
-
